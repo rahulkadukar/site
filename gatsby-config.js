1bbc6c3cb2ef6e03f41535b8f96a9b5b39f4c551
Rahul Kadukar <rahulkadukar@yahoo.com>
2018-11-14 21:34:18 -0500

    Basic Blog working, ready to determine issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,32 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
-    }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-katex`,
+           {
+          	resolve: `gatsby-remark-prismjs`,
+          	options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+           }
+          }
+        ],
+      },
+    },
   ],
   siteMetadata: {
     title: `Rahul Kadukar`,
---
+++ src/md/2018-11-08_hello-world.md
@@ -0,0 +1,7 @@
+---
+title: 'Hello World'
+date: '2018-11-08'
+tags: ['intro', '2018', 'november', '2018-November']
+---
+
+This is my first post. I will let it remain here for historical purposes.
